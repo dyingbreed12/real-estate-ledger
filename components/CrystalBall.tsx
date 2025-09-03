2ca17fc90f419d14663e24985fb961a124cc9f7c
Lee Egino <lee@invoco.net>
2025-09-03 13:23:47 +0800

    crystall ball description amend

--- components/CrystalBall.tsx
+++ components/CrystalBall.tsx
@@ -66,7 +66,7 @@ export default function CrystalBall({
               isAnimating ? "animate-fade-out" : ""
             }`}
           >
-            Predict the Profits
+            Predict Profits
           </span>
         )}
 
